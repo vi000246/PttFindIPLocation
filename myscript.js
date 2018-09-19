bd250de4d84c217a3e59c3882441671fadcf1b76
Yich <vi000246@gmail.com>
2018-09-19 17:59:36 +0800

    123

--- myscript.js
+++ myscript.js
@@ -3,8 +3,25 @@ $('html').on('keyup keypress mousedown', function(e) {
     var reg = /(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\s(\d{2}\/\d{2}\s\d{2}:\d{2})/g;
     var result;
     while((result = reg.exec(html)) !== null) {
-        console.log(result[1]);//這是IP
-        console.log(result[2]);//這是日期
-        $('#mainContainer').find("span[data-type='bbsline']").find('span').find('span:contains("'+result[1]+'")').text("hello"+result[1]+" "+result[2]);
+        var ip = result[1];
+        var bbsDate = result[2];
+        console.log(ip);//這是IP
+        console.log(bbsDate);//這是日期
+
+        $.ajax({
+            url: "https://extreme-ip-lookup.com/json/"+ip,
+            async:false,
+            success: function(data) {
+                if(data.countryCode){
+                    //總長度一定要29字元
+                    var newContent = (ip+"("+data.countryCode+") "+bbsDate).padEnd(29, ' ');
+                    var currentLine = $('#mainContainer').find("span[data-type='bbsline']").find('span').find('span:contains("'+ip+'")');
+                    currentLine.text(newContent);
+                }
+            }
+        });
+
+        
     }
 });
+
